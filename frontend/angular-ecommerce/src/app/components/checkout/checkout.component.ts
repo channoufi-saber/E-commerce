7b3ebc6ee404cb1e12bae83667968c08d5532841
channoufi-saber <saber.channoufi@esprit.tn>
2023-12-17 12:12:05 +0100

    Checkout Form Save the Order to Database Frontend

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -5,6 +5,11 @@ import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 import { CartService } from 'src/app/services/cart.service';
+import { CheckoutService } from 'src/app/services/checkout.service';
+import { Router } from '@angular/router';
+import { Order } from 'src/app/common/order';
+import { OrderItem } from 'src/app/common/order-item';
+import { Purchase } from 'src/app/common/purchase';
 
 
 @Component({
@@ -25,8 +30,10 @@ export class CheckoutComponent implements OnInit {
   billingAddressStates:State[]=[];
 
   constructor(private formBuilder:FormBuilder,
-    private luv2ShopFormService:Luv2ShopFormService,
-    private cartService:CartService) { }
+              private luv2ShopFormService:Luv2ShopFormService,
+              private cartService:CartService,
+              private checkoutService:CheckoutService,
+              private router:Router) { }
 
   ngOnInit(): void {
     this.reviewCartDetails();
@@ -155,11 +162,74 @@ export class CheckoutComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log("Handling the submit button");
     if(this.checkoutFormGroup.invalid){
       this.checkoutFormGroup.markAllAsTouched();
+      return;
     }
-    console.log(this.checkoutFormGroup.get('customer')!.value.email);
+
+    // Set up Order
+
+     let order = new Order(this.totalQuantity, this.totalPrice);
+
+    // get cart items
+    const cartItems=this.cartService.cartItems;
+
+    // create orderItems from cartItems
+    let orderItems: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem.imageUrl!, tempCartItem.unitPrice!, tempCartItem.quantity, tempCartItem.id!));
+
+     // set up purchase
+    let purchase = new Purchase();
+    
+    // populate purchase - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+    
+    // populate purchase - shipping address
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingState: State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+    const shippingCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+    purchase.shippingAddress.state = shippingState.name;
+    purchase.shippingAddress.country = shippingCountry.name;
+
+    // populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState: State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+    const billingCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.state = billingState.name;
+    purchase.billingAddress.country = billingCountry.name;
+  
+    // populate purchase - order and orderItems
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    // 
+
+    this.checkoutService.placeOrder(purchase).subscribe({
+      next:response =>{
+        alert(`Your order has been received.\n Order tracking number: ${response.orderTrackingNumber}`);
+
+        // reset cart
+        this.resetCart();
+      },
+      error:err =>{
+        alert(`there was an error : ${err.message}`);
+      }
+    })
+
+  }
+
+  resetCart(){
+    //reser cart data
+    this.cartService.cartItems=[];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    //reset the form
+
+    this.checkoutFormGroup.reset();
+
+    //navigate back to products page
+
+    this.router.navigateByUrl("/products");
   }
 
   handleMonthsAndYears(){
