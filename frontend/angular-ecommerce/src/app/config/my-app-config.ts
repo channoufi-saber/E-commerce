97f6f03ab1efc94307108da65740f675f6aa041d
channoufi-saber <saber.channoufi@esprit.tn>
2023-12-18 16:01:53 +0100

    Login Logout

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oab6h1qw8p3Tnr8s5d7',
+        issuer: 'https://dev-05091953.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
